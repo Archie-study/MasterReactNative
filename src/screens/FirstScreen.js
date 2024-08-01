4b5b0ecec204b98fcdc85c2f4a5a1ddad7906127
Archie-study <archiestudy12@gmail.com>
2024-08-01 11:53:35 +0700

    Meeting 1 - Practice Session

---
+++ src/screens/FirstScreen.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { View, Text, Button } from 'react-native'
+import { color } from "react-native-reanimated";
+
+const FirstScreen = () => {
+    const openAlert = () => {
+        alert('You clicked the button!')
+    }
+    return (
+        <View style={{
+            flex: 1,
+            alignItems: 'center',
+            justifyContent: 'center'
+        }}>
+            <Text style={{margin: 16}}>
+                Hello <Text style={{ color: 'red'}}>World!</Text>
+            </Text>
+            <Button title="This is button" onPress={openAlert} />
+        </View>
+    )
+}
+
+export default FirstScreen;
