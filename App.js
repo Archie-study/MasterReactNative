4b15822daf1566383a63f248756a0e138ecceda8
Archie-study <archiestudy12@gmail.com>
2024-08-01 12:03:48 +0700

    Meeting 1 - DIY Session

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import FirstScreen from "./src/screens/FirstScreen";
+import ChallengeScreenM1 from "./src/screens/FirstScreen";
 
 const App = () => {
   return (
     <SafeAreaProvider>
-      <FirstScreen />
+      <ChallengeScreenM1 />
     </SafeAreaProvider>
   )
 }
--- src/screens/FirstScreen.js
+++ src/screens/FirstScreen.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { View, Text, Button } from 'react-native'
-import { color } from "react-native-reanimated";
+
 
 const FirstScreen = () => {
     const openAlert = () => {
@@ -20,4 +20,19 @@ const FirstScreen = () => {
     )
 }
 
-export default FirstScreen;
+const ChallengeScreenM1 = () => {
+    const showAlert = () => {
+        alert('You are awesome!!!')
+    }
+    return (
+        <View style={{flex: 1, alignItems:'center', justifyContent: 'center'}}>
+            <Text style={{color: 'green', margin: 10}}>Let's Learn React Native Framework</Text>
+            <Text style={{margin: 10}}>My name is Michael</Text>
+            <Text style={{textAlign:'center', margin: 10}}>Now I'm learning about <Text style={{color: 'blue'}}>React Native Components</Text> to build the user interface for android apps</Text>
+            <Text style={{color: 'red', margin: 10}}>I Love Coding</Text>
+            <Button title="Click Me" onPress={showAlert} />
+        </View>
+    )
+}
+
+export default ChallengeScreenM1;
