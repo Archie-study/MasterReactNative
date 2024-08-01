78d286b21d382ce77ededd27eb1ddebfabcb1c13
Archie-study <archiestudy12@gmail.com>
2024-08-01 13:55:18 +0700

    Meeting 3 - Practice Session

--- App.js
+++ App.js
@@ -1,14 +1,16 @@
 import React from "react";
 import { SafeAreaProvider } from "react-native-safe-area-context";
-import FirstScreen from "./src/screens/FirstScreen";
-import ChallengeScreenM1 from "./src/screens/FirstScreen";
-import SecondScreen from "./src/screens/SecondScreen";
-import ChallengeScreenM2 from "./src/screens/SecondScreen";
+// import FirstScreen from "./src/screens/FirstScreen";
+// import ChallengeScreenM1 from "./src/screens/FirstScreen";
+// import SecondScreen from "./src/screens/SecondScreen";
+// import ChallengeScreenM2 from "./src/screens/SecondScreen";
+import ThirdScreen from "./src/screens/ThirdScreen";
+
 
 const App = () => {
   return (
     <SafeAreaProvider>
-      <ChallengeScreenM2 />
+      <ThirdScreen />
     </SafeAreaProvider>
   )
 }
---
+++ src/screens/ThirdScreen.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { View, Text } from 'react-native'
+
+const ThirdScreen = () => {
+    return (
+        <View style={{backgroundColor:'red', flex: 1}}>
+            <Text>
+                Pikachu
+            </Text>
+        </View>
+
+    )
+}
+
+export default ThirdScreen;
