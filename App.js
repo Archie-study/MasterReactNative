016644e356fe7f656effc82a646f736ab565119c
Archie-study <archiestudy12@gmail.com>
2024-08-01 12:09:14 +0700

    Meeting 2 - Practice Session

--- App.js
+++ App.js
@@ -2,11 +2,12 @@ import React from "react";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import FirstScreen from "./src/screens/FirstScreen";
 import ChallengeScreenM1 from "./src/screens/FirstScreen";
+import SecondScreen from "./src/screens/SecondScreen";
 
 const App = () => {
   return (
     <SafeAreaProvider>
-      <ChallengeScreenM1 />
+      <SecondScreen />
     </SafeAreaProvider>
   )
 }
---
+++ src/screens/SecondScreen.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { View, Text } from 'react-native'
+
+const SecondScreen = () => {
+    return (
+        <View>
+            <View>
+                <Text>Mobile Legends</Text>
+            </View>
+
+            <View>
+                <Text>PUBG</Text>
+            </View>
+        </View>
+    )
+}
+
+export default SecondScreen
