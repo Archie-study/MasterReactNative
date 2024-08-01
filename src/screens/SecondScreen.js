a1f16378f4e1cf527d52589b345d1c26da8626f0
Archie-study <archiestudy12@gmail.com>
2024-08-01 12:13:27 +0700

    Meeting 2 - Challenge 1

--- src/screens/SecondScreen.js
+++ src/screens/SecondScreen.js
@@ -3,12 +3,12 @@ import { View, Text } from 'react-native'
 
 const SecondScreen = () => {
     return (
-        <View>
-            <View>
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: 'lavender'}}>
+            <View style={{backgroundColor: 'skyblue'}}>
                 <Text>Mobile Legends</Text>
             </View>
 
-            <View>
+            <View style={{backgroundColor: 'cornflowerblue'}}>
                 <Text>PUBG</Text>
             </View>
         </View>
