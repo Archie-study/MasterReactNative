29003afc554b0e2a4549901a6eebc06b3d458d0d
Archie-study <archiestudy12@gmail.com>
2024-08-01 14:02:03 +0700

    Meeting 3 - Challenge 1

--- src/screens/ThirdScreen.js
+++ src/screens/ThirdScreen.js
@@ -3,10 +3,26 @@ import { View, Text } from 'react-native'
 
 const ThirdScreen = () => {
     return (
-        <View style={{backgroundColor:'red', flex: 1}}>
-            <Text>
-                Pikachu
-            </Text>
+        <View style={{backgroundColor:'mistyrose', flex: 1}}>
+            <View style={{backgroundColor: 'red', flex: 1}}>
+                <Text style={{color: 'white'}}>
+                    Pikachu
+                </Text>
+            </View>
+
+
+            <View style={{backgroundColor: 'orange', flex: 2}}>
+                <Text>
+                    Flareon
+                </Text>
+            </View>
+
+
+            <View style={{backgroundColor: 'yellow', flex: 3}}>
+                <Text>
+                    Rapidash
+                </Text>
+            </View>
         </View>
 
     )
