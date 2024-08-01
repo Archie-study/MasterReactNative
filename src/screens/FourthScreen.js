522e2e73fd933a0e2eb75dbb4be29a6975d74b66
Archie-study <archiestudy12@gmail.com>
2024-08-01 15:20:28 +0700

    Meeting 4 - Challenge 2

--- src/screens/FourthScreen.js
+++ src/screens/FourthScreen.js
@@ -1,48 +1,71 @@
 import React from "react";
-import { View, Text, ImageBackground, TextInput } from 'react-native'
+import { View, Text, ImageBackground, TextInput, ScrollView } from 'react-native'
 
 const FourthScreen = () => {
     return (
-        <View style={{margin: 16}}>
-            <ImageBackground 
-                style={{
-                    width: '100%',
-                    height: 300,
-                    justifyContent: 'flex-end',
-                    alignItems: 'flex-start'
-                }}
-                imageStyle={{ borderRadius: 10, opacity: 0.5}}
-                source={{ uri: 'https://wallpaperaccess.com/full/250180.jpg'}}
-            >
-                <Text 
+        <ScrollView>
+            <View style={{margin: 16}}>
+                <ImageBackground 
                     style={{
-                        backgroundColor:'mistyrose',
-                        padding: 8,
-                        margin: 8,
-                        fontSize: 18,
-                        fontWeight: 'bold'
+                        width: '100%',
+                        height: 300,
+                        justifyContent: 'flex-end',
+                        alignItems: 'flex-start'
                     }}
+                    imageStyle={{ borderRadius: 10, opacity: 0.5}}
+                    source={{ uri: 'https://wallpaperaccess.com/full/250180.jpg'}}
                 >
-                    Disneyland
+                    <Text 
+                        style={{
+                            backgroundColor:'mistyrose',
+                            padding: 8,
+                            margin: 8,
+                            fontSize: 18,
+                            fontWeight: 'bold'
+                        }}
+                    >
+                        Disneyland
+                    </Text>
+                </ImageBackground>
+
+
+                {/* DESCRIPTION */}
+                <Text style={{ marginTop:12, marginBottom: 12}}>
+                    Description
                 </Text>
-            </ImageBackground>
 
-            <Text style={{ marginTop:12, marginBottom: 12}}>
-                Description
-            </Text>
+                <TextInput 
+                    style={{
+                        padding:10,
+                        borderWidth: 1,
+                        height: 140,
+                        fontSize: 16
+                    }}
+                    placeholder="About the place"
+                    multiline={true}
+                    keyboardType="default"
+                />
 
-            <TextInput 
-                style={{
-                    padding:10,
-                    borderWidth: 1,
-                    height: 40,
-                    fontSize: 16
-                }}
-                placeholder="About the place"
-                multiline={true}
-                keyboardType="default"
-            />
-        </View>
+
+
+                {/* PHONE NUMBER */}
+                <Text style={{ marginTop:12, marginBottom: 12}}>
+                    Phone Number
+                </Text>
+
+                <TextInput 
+                    style={{
+                        padding:10,
+                        borderWidth: 1,
+                        height: 40,
+                        fontSize: 16
+                    }}
+                    placeholder="Phone Number"
+                    multiline={true}
+                    keyboardType="phone-pad"
+                />
+            </View>
+        </ScrollView>
     )
 }
 
