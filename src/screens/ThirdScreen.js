6150c9f0073fc307178c32102436d029aa02a125
Archie-study <archiestudy12@gmail.com>
2024-08-01 14:41:04 +0700

    Meeting 3 - Challenge 2

--- src/screens/ThirdScreen.js
+++ src/screens/ThirdScreen.js
@@ -30,7 +30,9 @@ const ThirdScreen = () => {
 
 const AddImage = () => {
     return (
-        <View>
+        <View style={{alignItems: 'center'}}>
+            
+            {/* Pikachu */}
             <View style={{ margin: 8, padding: 8 }}>
                 <Image style={{
                     width: 150,
@@ -43,6 +45,70 @@ const AddImage = () => {
                 source={require('../../assets/images/pikachu.png')}
                 />
             </View>
+
+
+            {/* Flareon */}
+            <View style={{ margin: 8, padding: 8 }}>
+                <Image style={{
+                    width: 150,
+                    height: 150,
+                    borderRadius: 100,
+                    borderWidth: 1,
+                    borderColor: 'green',
+                    backgroundColor: 'aliceblue'
+                }} 
+                source={require('../../assets/images/flareon.png')}
+                />
+            </View>
+
+
+            {/* Rapidash */}
+            <View style={{ margin: 8, padding: 8 }}>
+                <Image style={{
+                    width: 150,
+                    height: 150,
+                    borderRadius: 100,
+                    borderWidth: 1,
+                    borderColor: 'green',
+                    backgroundColor: 'aliceblue'
+                }} 
+                source={require('../../assets/images/rapidash.png')}
+                />
+            </View>
+
+
+            {/* Psyduck */}
+            <View style={{ margin: 8, padding: 8 }}>
+                <Image style={{
+                    width: 150,
+                    height: 150,
+                    borderRadius: 100,
+                    borderWidth: 1,
+                    borderColor: 'green',
+                    backgroundColor: 'aliceblue'
+                }} 
+                source={{uri: 'https://id.portal-pokemon.com/play/resources/pokedex/img/pm/0783062d0d860b8ae7d8e859241a700359c4d981.png'}}
+                />
+            </View>
+
+
+            {/* Bulbasaur */}
+            <View style={{ margin: 8, padding: 8 }}>
+                <Image style={{
+                    width: 150,
+                    height: 150,
+                    borderRadius: 100,
+                    borderWidth: 1,
+                    borderColor: 'green',
+                    backgroundColor: 'aliceblue'
+                }} 
+                source={{uri: 'https://img.pokemondb.net/artwork/large/bulbasaur.jpg'}}
+                />
+            </View>
+
+
+
+
         </View>
     )
 }
