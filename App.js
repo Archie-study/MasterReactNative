c7bfb412a8bdf142c084a4b138ed6a2fb2169fef
Archie-study <archiestudy12@gmail.com>
2024-09-18 11:29:23 +0700

    Finalizing Meeting 1

--- App.js
+++ App.js
@@ -1,21 +1,12 @@
 import React from "react";
+import 'react-native-gesture-handler'
 import { SafeAreaProvider } from "react-native-safe-area-context";
-// import FirstScreen from "./src/screens/FirstScreen";
-// import ChallengeScreenM1 from "./src/screens/FirstScreen";
-// import SecondScreen from "./src/screens/SecondScreen";
-// import ChallengeScreenM2 from "./src/screens/SecondScreen";
-// import ThirdScreen from "./src/screens/ThirdScreen";
-// import AddImage from "./src/screens/ThirdScreen";
-// import HorizontalImage from "./src/screens/ThirdScreen";
-// import ChallengeScreenM3 from "./src/screens/ThirdScreen";
-// import FourthScreen from "./src/screens/FourthScreen";
-// import ChallengeScreenM4 from "./src/screens/FourthScreen";
-import InstagramHomeScreen from "./src/screens/InstagramHomeScreen";
+import MainNavigator from "./src/navigator/MainNavigator";
 
 const App = () => {
   return (
     <SafeAreaProvider>
-      <InstagramHomeScreen />
+      <MainNavigator />
     </SafeAreaProvider>
   )
 }
--- src/components/InputComponent.js
+++ src/components/InputComponent.js
@@ -1,23 +1,45 @@
-import React from "react";
-import { View, Text, TextInput } from 'react-native'
+import React from 'react'
+import { 
+    View,
+    Text,
+    TextInput,
+    StyleSheet 
+} from 'react-native'
 
-export const InputComponent = (props) => {
-    const { title, height } = props;
-
+export const Input = (props) => {
+    const { title } = props;
     return (
-        <View>
-            <Text style={{marginTop:12, marginBottom:12}}>
-                {title}
-            </Text>
-            <TextInput
-                style={{
-                   padding: 10,
-                   borderWidth: 1, 
-                   height: height,
-                   fontSize: 16
-                }} 
-                {...props}
-            />
+
+        <View style={styles.mainContainer}>
+            <View style={styles.titleContainer}>
+                <Text>{title}</Text>
+            </View>
+            <View style={styles.inputContainer}>
+                <TextInput 
+                    style={styles.input}
+                    {...props}
+                />
+            </View>
         </View>
+
     )
 }
+
+const styles = StyleSheet.create({
+    mainContainer: {
+        marginBottom: 8,
+    },
+    titleContainer: {
+        marginLeft: 16
+    },
+    inputContainer: {
+        borderWidth: 1,
+        borderRadius: 20,
+        margin: 8
+    },
+    input: {
+        padding: 8,
+        flex: 1,
+        color: 'black'
+    }
+})
